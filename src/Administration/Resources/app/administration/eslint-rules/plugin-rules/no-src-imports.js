4036294af0e0694113669da1e8b427f4c2b34341
gxs <lyricsong47@gmail.com>
2025-09-10 16:07:34 +0800

    refactor: added indexes to order tables

---
+++ src/Administration/Resources/app/administration/eslint-rules/plugin-rules/no-src-imports.js
@@ -0,0 +1,24 @@
+/**
+ * @sw-package framework
+ */
+
+/* eslint-disable max-len */
+module.exports = {
+    create(context) {
+        return {
+            ImportDeclaration(node) {
+                const invalidNodeSources = [];
+                invalidNodeSources.push(node.source.value.startsWith('@administration/'));
+
+                if (invalidNodeSources.includes(true)) {
+                    context.report({
+                        loc: node.source.loc.start,
+                        message: `\
+You can't use imports directly from the HeyFrame Core via "${node.source.value}". \
+Use the global HeyFrame object directly instead (https://developer.heyframe.net/docs/guides/plugins/plugins/administration/the-heyframe-object)`,
+                    });
+                }
+            },
+        };
+    },
+};
