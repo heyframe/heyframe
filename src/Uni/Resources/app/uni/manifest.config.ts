d4139e005530018bc123a9afaddf2fbf0ebbd96b
gxs <lyricsong47@gmail.com>
2025-09-17 16:07:27 +0800

    feat: add svg icon

--- src/Uni/Resources/app/uni/manifest.config.ts
+++ src/Uni/Resources/app/uni/manifest.config.ts
@@ -24,7 +24,6 @@ export default defineManifestConfig({
   'description': '',
   'versionName': '1.0.0',
   'versionCode': '100',
-  'transformPx': false,
   'locale': VITE_FALLBACK_LOCALE, // 'zh-Hans'
   'h5': {
     router: {
