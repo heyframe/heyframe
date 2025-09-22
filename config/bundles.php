fb71efc02eb6f74da28016f137be2cc51d39553a
gxs <lyricsong47@gmail.com>
2025-09-22 22:17:28 +0800

    feat: add pay framework

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,7 @@ $bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    HeyPay\Bundle\PayBundle\PayBundle::class => ['all' => true],
     HeyFrame\Core\Profiling\Profiling::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     HeyFrame\Core\Framework\Framework::class => ['all' => true],
