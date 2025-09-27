366020c6d97ec7418fc116381a8b0b16760a9d0c
gxs <lyricsong47@gmail.com>
2025-09-27 17:23:25 +0800

    init

--- src/Uni/Resources/app/uni/vite.config.ts
+++ src/Uni/Resources/app/uni/vite.config.ts
@@ -1,91 +1,32 @@
-import {defineConfig, loadEnv} from "vite";
 import Uni from '@uni-helper/plugin-uni'
-import * as path from "node:path";
-import colors from 'picocolors';
-import UniManifest from '@uni-helper/vite-plugin-uni-manifest'
-import UniComponents from '@uni-helper/vite-plugin-uni-components'
-import {NutResolver} from "@heyframe/nutui-uniapp";
-import AutoImport from 'unplugin-auto-import/vite'
+import UniHelperComponents from '@uni-helper/vite-plugin-uni-components'
+import UniHelperLayouts from '@uni-helper/vite-plugin-uni-layouts'
+import UniHelperManifest from '@uni-helper/vite-plugin-uni-manifest'
+import UniHelperPages from '@uni-helper/vite-plugin-uni-pages'
 import UnoCSS from 'unocss/vite'
-import UniPages from '@uni-helper/vite-plugin-uni-pages'
-import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
-export default defineConfig(({command, mode}) => {
-  const isProd = command === 'build';
-  const isDev = !isProd;
-  const {UNI_PLATFORM, UNI_PLUGIN_NAME} = process.env
+import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite'
+import UniPolyfill from 'vite-plugin-uni-polyfill'
 
-  const base = isProd ? `/bundles/${UNI_PLUGIN_NAME}/uni` : undefined;
-
-
-  console.log(colors.yellow(`UNI_PLATFORM: ${UNI_PLATFORM}`))
-  console.log(colors.yellow(`UNI_PLUGIN_NAME: ${UNI_PLUGIN_NAME}`))
-  console.log(colors.yellow(`APP_URL: ${process.env.APP_URL}`))
-
-
-  const env = loadEnv(mode, path.resolve(process.cwd()))
-  const {VITE_APP_PORT} = env
-  return {
-    base,
-    plugins: [
-      UniPages({
-        exclude: ['**/components/**/**.*'],
-        dts: 'src/types/uni-pages.d.ts',
-        minify: true,
-      }),
-      UniLayouts(),
-      UniManifest(),
-      UniComponents({
-        deep: true,
-        dts: 'src/components.d.ts',
-        resolvers: [NutResolver()],
-      }),
-      UnoCSS(),
-      AutoImport({
-        imports: [
-          'vue',
-          'pinia',
-          'uni-app',
-          {
-            '@heyframe/nutui-uniapp/composables': [
-              'useNotify',
-              'useToast',
-            ],
-          },
-        ],
-        dts: 'src/auto-imports.d.ts',
-        dirs: ['src/app/composables', 'src/app/stores'],
-        vueTemplate: true,
-      }),
-      Uni(),
-    ],
-    resolve: {
-      alias: {
-        '@': path.join(process.cwd(), './src'),
-      },
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@import "@heyframe/nutui-uniapp/styles/variables";',
-        },
-      },
-    },
-    server: {
-      hmr: true,
-      host: process.env.HOST ? process.env.HOST : 'localhost',
-      port: Number(VITE_APP_PORT) || 9000,
-      proxy: isDev ? {
-        '/front-api': {
-          target: process.env.APP_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-      }:undefined,
-    },
-    build: {
-      sourcemap: false,
-      target: 'es6',
-      minify: mode === 'development' ? false : 'esbuild',
-    }
-  }
-});
+export default defineConfig({
+  plugins: [
+    UniHelperManifest(),
+    UniHelperPages({
+      dts: 'src/uni-pages.d.ts',
+    }),
+    UniHelperLayouts(),
+    UniHelperComponents({
+      dts: 'src/components.d.ts',
+      directoryAsNamespace: true,
+    }),
+    Uni(),
+    UniPolyfill(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core', 'uni-app'],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src/app/composables', 'src/app/stores'],
+      vueTemplate: true,
+    }),
+    UnoCSS(),
+  ],
+})
