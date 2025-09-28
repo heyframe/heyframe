a1aacf5c80e3f5a2c26eb77fa9969e495f713292
gxs <lyricsong47@gmail.com>
2025-09-28 14:59:30 +0800

    fix: Add correct mapping for en-GB and zh-CN

--- src/Core/System/Snippet/SnippetService.php
+++ src/Core/System/Snippet/SnippetService.php
@@ -314,7 +314,12 @@ class SnippetService
     {
         $result = [];
         foreach ($isoList as $iso) {
-            $result[$iso] = $this->snippetFileCollection->getSnippetFilesByIso($iso);
+            $mappedIso = match ($iso) {
+                'zh-CN' => 'zh',
+                'en-GB' => 'en',
+                default => $iso,
+            };
+            $result[$iso] = $this->snippetFileCollection->getSnippetFilesByIso($mappedIso);
         }
 
         return $result;
