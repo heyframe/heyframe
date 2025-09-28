6f25f7e964a506665d30d302d0f82242d104fc53
gxiaosong <lyricsong47@gmail.com>
2025-09-28 12:06:49 +0800

    Update ScriptKernel.php

--- custom/scripts/boot/ScriptKernel.php
+++ custom/scripts/boot/ScriptKernel.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace Scripts\Boot;
-
+use HeyFrame\Core\Kernel;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class ScriptKernel extends \Shopware\Core\Kernel
+class ScriptKernel extends Kernel
 {
     protected function build(ContainerBuilder $container): void
     {
