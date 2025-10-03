961ed530080cd3c9fb3b1d133c6a56fb3d2acf9b
gxs <lyricsong47@gmail.com>
2025-10-03 12:00:05 +0800

    fix: Add specific cms state page property to watcher

--- src/Administration/Resources/app/administration/src/module/sw-cms/component/sw-cms-mapping-field/index.ts
+++ src/Administration/Resources/app/administration/src/module/sw-cms/component/sw-cms-mapping-field/index.ts
@@ -71,10 +71,20 @@ export default HeyFrame.Component.wrapComponentConfig({
     },
 
     watch: {
-        cmsPageState: {
-            deep: true,
+        'cmsPageState.currentMappingTypes': {
             handler() {
                 this.updateMappingTypes();
+            },
+        },
+
+        'cmsPageState.currentMappingEntity': {
+            handler() {
+                this.updateMappingTypes();
+            },
+        },
+
+        'cmsPageState.currentDemoEntity': {
+            handler() {
                 this.updateDemoValue();
             },
         },
