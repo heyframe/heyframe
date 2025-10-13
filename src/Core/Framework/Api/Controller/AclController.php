66149cdd179599c8d26ff7bcfcb78734d69beeac
gxs <lyricsong47@gmail.com>
2025-10-13 21:30:21 +0800

    feat: Enable PHP files for components

--- src/Core/Framework/Api/Controller/AclController.php
+++ src/Core/Framework/Api/Controller/AclController.php
@@ -2,7 +2,7 @@
 
 namespace HeyFrame\Core\Framework\Api\Controller;
 
-use HeyFrame\Core\Framework\Api\Acl\Admin\Event\AclGetAdditionalPrivilegesEvent;
+use HeyFrame\Core\Framework\Api\Acl\Event\AclGetAdditionalPrivilegesEvent;
 use HeyFrame\Core\Framework\Api\Acl\Role\AclRoleDefinition;
 use HeyFrame\Core\Framework\Context;
 use HeyFrame\Core\Framework\DataAbstractionLayer\DefinitionInstanceRegistry;
